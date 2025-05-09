6d5f74e0c45df7e03c4ecaa644a2d0b66ae3f6c2
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-05-09 14:46:50 +0100

    Ex 1

---
+++ IntCollections/Program.cs
@@ -0,0 +1,33 @@
+﻿﻿using System;
+using System.Collections.Generic;
+
+namespace IntCollections
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] valores = { 1, 10, -30, 10, -5 };
+
+            List<int> lista = new List<int>();
+            foreach (int valor in valores)
+                lista.Add(valor);
+            Console.WriteLine("List: " + string.Join(", ", lista));
+
+            Stack<int> pilha = new Stack<int>();
+            foreach (int valor in valores)
+                pilha.Push(valor);
+            Console.WriteLine("Stack: " + string.Join(", ", pilha));
+
+            Queue<int> fila = new Queue<int>();
+            foreach (int valor in valores)
+                fila.Enqueue(valor);
+            Console.WriteLine("Queue: " + string.Join(", ", fila));
+
+            HashSet<int> conjunto = new HashSet<int>();
+            foreach (int valor in valores)
+                conjunto.Add(valor);
+            Console.WriteLine("HashSet: " + string.Join(", ", conjunto));
+        }
+    }
+}
