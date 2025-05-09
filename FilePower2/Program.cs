57a737732d97fda6947a6d0f5e5b2b47867b28ff
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-05-09 15:01:15 +0100

    Ex 6

---
+++ FilePower2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("not enough arguments");
+                return;
+            }
+
+            string fileName = args[0];
+
+            using StreamWriter sw = new StreamWriter(fileName);
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                    break;
+
+                sw.WriteLine(input);
+            }
+        }
+    }
+}
