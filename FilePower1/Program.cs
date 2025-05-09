a41d9e6cca8754b59d66fc0a4bd8f10f0cd8bcf8
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-05-09 14:55:52 +0100

    Ex 5

---
+++ FilePower1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("not enough arguments");
+                return;
+            }
+
+            string fileName = args[0];
+            Queue<string> queue = new Queue<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                    break;
+                queue.Enqueue(input);
+            }
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                while (queue.Count > 0)
+                {
+                    string line = queue.Dequeue();
+                    writer.WriteLine(line);
+                }
+            }
+        }
+    }
+}
